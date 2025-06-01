6685f4549a7868f38bb200b734feba05e6cc77ad
jaeyeopme <jaeyeopme@gmail.com>
2025-06-01 20:50:36 +0900

    feat: axios API 개발 서버 프록시 추가

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,16 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target:
+          "http://scuba-management-backend-env.eba-8pjmhaiu.ap-northeast-2.elasticbeanstalk.com",
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
