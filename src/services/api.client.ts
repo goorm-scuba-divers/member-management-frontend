1c3ad5f91149ab1a1597e93fdde9c80ca30ed228
jaeyeopme <jaeyeopme@gmail.com>
2025-06-14 01:00:41 +0900

    feat: API 클라이언트 응답 인터셉터 추가 및 인증 처리 개선
    - Axios 401 및 403 상태 코드에 대한 처리 로직 구현
    - 403 시 로그인 페이지로 리디렉션 및 요청 큐 관리 기능 추가
    - 요청 실패 시 재시도 로직을 통해 사용자 경험 향상

--- src/services/api.client.ts
+++ src/services/api.client.ts
@@ -1,9 +1,85 @@
-import axios from "axios"
+import { useAuthStore } from "@/store/auth.store"
+import axios, { type AxiosError, type AxiosResponse, type InternalAxiosRequestConfig } from "axios"
 
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
   headers: {
-    "Content-Type": "application/json",
+    contentType: "application/json",
   },
   withCredentials: true,
 })
+
+let isRefreshing = false
+
+let failedQueue: Array<{
+  resolve: (value: unknown) => void
+  reject: (error: unknown) => void
+}> = []
+
+const redirectToLogin = () => {
+  useAuthStore.getState().signout()
+  window.location.href = "/signin"
+}
+
+/**
+ * 대기 중인 요청 큐를 처리하는 함수
+ *
+ * @param error - 에러가 있으면 큐의 모든 요청에 에러를 전파, 없으면(null) 모든 요청을 성공 처리
+ */
+const processQueue = (error: Error | null) => {
+  // 큐에 있는 모든 요청 처리
+  for (const request of failedQueue) {
+    if (error) {
+      request.reject(error)
+    } else {
+      request.resolve(null)
+    }
+  }
+
+  failedQueue = []
+}
+
+apiClient.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  async (error: AxiosError) => {
+    const { status } = error.response || {}
+
+    const originalRequest = error.config as InternalAxiosRequestConfig & { _retry?: boolean }
+
+    // UNAUTHORIZED
+    if (status === 401) {
+      redirectToLogin()
+      return Promise.reject(error)
+    }
+
+    // FORBIDDEN
+    if (status === 403 && !originalRequest._retry) {
+      originalRequest._retry = true
+
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => failedQueue.push({ resolve, reject }))
+          .then(() => apiClient(originalRequest))
+          .catch(err => Promise.reject(err))
+      }
+
+      try {
+        isRefreshing = true
+
+        await apiClient.post("/auth/reissue")
+
+        processQueue(null)
+
+        isRefreshing = false
+
+        return apiClient(originalRequest)
+      } catch (refreshError) {
+        processQueue(refreshError as Error)
+        redirectToLogin()
+        isRefreshing = false
+        return Promise.reject(refreshError)
+      }
+    }
+
+    return Promise.reject(error)
+  }
+)
